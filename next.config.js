09f0c2da7683aaa81f4fcf881e24cdce399a6744
severussnapz <leemalemusic@gmail.com>
2026-05-13 11:18:49 +0100

    Remove CSP config to allow Next.js to work properly

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' https://api.openai.com;",
-          },
-        ],
-      },
-    ]
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
